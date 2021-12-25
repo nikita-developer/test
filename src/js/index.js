231087fd4aef2a33f5d7006de9b810c6667647d2
Nikita <cycyrbita@mail.ru>
2021-12-25 19:24:20 +0300

    правки

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
+    // бургер меню
     $('.js-nav__burger').click(function () {
         $('.js-nav__body').toggleClass('visible');
         $('html, body').toggleClass('overflow');
     });
 
+    // стилизация select
     $('select, input[type="file"]').styler();
+
+    // подставляем значение ползунка
+    var range = document.querySelector('.js-form__range');
+    range.addEventListener('input', function () {
+        $('.js-form__info span').text($(this).val() + '%');
+    }, false);
 });
